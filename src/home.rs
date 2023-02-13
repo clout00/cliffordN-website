fecaf68fd23a30f84a887e086c042977e05b0ae7
Clifford <cnkarenbi@gmail.com>
2023-02-13 13:47:29 +0200

    new styles

--- src/home.rs
+++ src/home.rs
@@ -2,19 +2,88 @@ use yew::prelude::*;
 
 #[function_component]
 pub fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
+    
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="container">
+
+        <div id="navigation-bar">
+          <nav>
+            <ul>
+              <li>{<a href="#" id="logo">LOGO</a>}</li>
+              <li>{<a href="#">Home</a>}</li>
+              <li><a href=#>About</a></li>
+              <li><a href=#>Portfolio</a></li>
+              <li><a href=#>Services</a></li>
+              <li><a href=#>Contact</a></li>
+              <a href="#" id="menu-icon"></a>
+            </ul>
+          </nav>
         </div>
+      
+        <div class="content">
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+          <p>Some text some text some text some text..</p>
+        </div>
+      
+      </div>
     }
+  
 }
